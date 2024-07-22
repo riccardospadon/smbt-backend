4d52bae05e68e0d7fddb695a560a7908f9a9eaf6
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-07-22 21:24:24 +0200

    aggiunta pacchetto 'CORS' + configurazione

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
-import express from 'express';
-import { genericError } from './middlewares/genericError.js';
-import apiRouter from './routers/apiRouter.js';
+import express from "express";
+import { genericError } from "./middlewares/genericError.js";
+import apiRouter from "./routers/apiRouter.js";
+import cors from 'cors'
 
-const server = express()
+const server = express();
 
 // porta del server
-const port = 3050
+const port = 3050;
+
+// configurazione di CORS
+const whitelist = ["http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, next) {
+    if (whitelist.includes(origin) || !origin) {
+      next(null, true);
+    } else {
+      next(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+server.use(cors(corsOptions))
 
 // Middleware per analizzare i corpi delle richieste JSON
-server.use(express.json())
+server.use(express.json());
 
 // Sotto-directory /api
-server.use('/api', apiRouter)
+server.use("/api", apiRouter);
 
 // Errore generico (gestito nella cartella "middlewares")
-server.use(genericError)
+server.use(genericError);
 
 // Controllo stato del server (visto su Postman)
-server.get('/health', (req, res) => {
-    res.status(200).json({ message: 'OK' })
-})
+server.get("/health", (req, res) => {
+  res.status(200).json({ message: "OK" });
+});
 
 server.listen(port, () => {
-    console.log("🚀 Server listening on port " + port)
-})
+  console.log("🚀 Server listening on port " + port);
+});
