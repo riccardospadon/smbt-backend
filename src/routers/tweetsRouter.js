e65dcd58ecd82cbf593baf8d475f5fe727fb1e52
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-07-22 13:22:43 +0200

    Eliminato cartella models + implementato la route tweetsRouter

--- src/routers/tweetsRouter.js
+++ src/routers/tweetsRouter.js
@@ -2,10 +2,52 @@ import express from 'express';
 
 const tweetsRouter = express.Router();
 
+// Classe Tweet per definire la struttura dei tweet
+class Tweet {
+    constructor(text) {
+        this.id = Tweet.incrementId()
+        this.text = text
+        this.likes = 0
+    }
+
+    static incrementId() {
+        if(!this.latestId) this.latestId = 1
+        else this.latestId++
+        return this.latestId
+    }
+}
+
+// Struttura dati per memorizzare i tweet
+const tweets = []
+
 // GET per ritornare tutti i tweets
+tweetsRouter.get('/', (req, res) => {
+    res.json(tweets)
+})
 
 // POST per pubblicare un nuovo tweet
+tweetsRouter.post('/', (req, res) => {
+    const { text } = req.body   // testo richiesto 
+    if(!text) {
+        return res.status(400).json({ error: 'Testo necessario!' })
+    }
+
+    const newTweet = new Tweet(text)
+    tweets.push(newTweet)
+    res.status(200).json(newTweet)
+})
 
 // POST per incrementare il contatore dei "mi piace"
+tweetsRouter.post('/:id/like', (req, res) => {
+    const tweetId = parseint(req.params.id)
+    const tweet = tweets.find(t => t.id === tweetId)
+
+    if(!tweet){
+        return res.status(404).json({ error: 'Tweet non trovato! '})
+    }
+
+    tweet.likes++
+    res.json(tweet)
+})
 
 export default tweetsRouter
